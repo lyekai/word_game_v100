800f20f2a4c97b2069e2684a4c765a16a8d8fe3b
lyekai <eric.kuo2003@gmail.com>
2026-04-23 10:29:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -260,4 +260,4 @@ def generate_image():
             return jsonify({"error": "server_error"}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
